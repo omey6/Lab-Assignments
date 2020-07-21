3f710c54f5730a9a9d625bfa921792868ee8cc05
umar anwer <smu_420@hotmail.com>
2020-07-21 14:26:14 -0500

    Lab 11

---
+++ Lab 11/Lab 11/SakilaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace Lab_11
+{
+    class SakilaContext : DbContext
+    {
+
+        public DbSet<Film> Film { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=localhost\sqlexpress;Database=sakila;Trusted_Connection=True;")
+;
+        }
+    }
+}
