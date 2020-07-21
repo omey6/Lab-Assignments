761175b5f942ebaee06fea23a5fd482f4ff0fa27
umar anwer <smu_420@hotmail.com>
2020-07-21 12:46:47 -0500

    Lab 10

---
+++ Lab 10/Lab 10/IAudioPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_10
+{
+    internal interface IAudioPlayer
+    {
+        void Play();
+    }
+}
---
+++ Lab 10/Lab 10/MediaPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_10
+{
+    internal class MediaPlayer
+    {
+
+        public void PlayMedia(IAudioPlayer media)
+        {
+            media.Play();
+        }
+    }
+}
